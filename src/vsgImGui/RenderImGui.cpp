454511ece43c67a6803a4862d7b14ce7ea419b4f
Mauricio Franco <mfranco@moffattnichol.com>
2023-12-29 00:58:44 +0000

    Let the user create the ImGUI context so he/she can control the settings before the renderer is initialized.

--- src/vsgImGui/RenderImGui.cpp
+++ src/vsgImGui/RenderImGui.cpp
@@ -118,8 +118,8 @@ void RenderImGui::_init(
     VkExtent2D imageSize, bool useClearAttachments)
 {
     IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImPlot::CreateContext();
+    //ImGui::CreateContext();
+    //ImPlot::CreateContext();
 
     VkSampleCountFlagBits samples = VK_SAMPLE_COUNT_1_BIT;
     for (auto& attachment : renderPass->attachments)
